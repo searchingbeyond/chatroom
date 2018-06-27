afac263a559fbfa8c3320b57ce8b29070168c07c
searchingbeyond <1355036599@qq.com>
2018-06-27 11:41:04 +0800

    增加聊天界面的样式

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var ws = require("nodejs-websocket")
- 
+
 var port = 8001;
 var count = 0;//client的计数器
- 
+
 function broadcast(server, msg) {
-    //server.connections是一个数组，保存着所有client
+	//server.connections是一个数组，保存着所有client
     server.connections.forEach(function (connection) {
         connection.sendText(msg);
     });
@@ -14,31 +14,31 @@ var server = ws.createServer(function (conn) {
     count++;
     conn.nickname = "user" + count;
     var msg = {
-        "type" : "enter",
-        "data" : conn.nickname + " come in"
+    	"type" : "enter",
+    	"data" : conn.nickname + " come in"
     }
     broadcast(server, JSON.stringify(msg));
     conn.on("text", function (str) {
         console.log("Received From: "+ conn.nickname + " ----  Data:" + str);
         var msg = {
-            "type" : "message",
-            "data" : conn.nickname + ": " + str
-        }
-        broadcast(server, JSON.stringify(msg));
+	    	"type" : "message",
+	    	"data" : conn.nickname + ": " + str
+	    }
+	    broadcast(server, JSON.stringify(msg));
     });
     conn.on("close", function (code, reason) {
-        var msg = {
-            "type" : "left",
-            "data" : conn.nickname + " left the room"
-        }
-        broadcast(server, JSON.stringify(msg));
+    	var msg = {
+	    	"type" : "left",
+	    	"data" : conn.nickname + " left the room"
+	    }
+	    broadcast(server, JSON.stringify(msg));
         console.log("Connection closed");
     });
     conn.on("error", function(err){
-        console.log("some accident happend");
-        console.log(err);
+    	console.log("some accident happend");
+    	console.log(err);
     });
 });
- 
+
 console.log("websocket started , listening " + port)
 server.listen(port)
--- websocket.js
+++ websocket.js
@@ -2,32 +2,41 @@ var info = document.getElementById("info");
 var send= document.getElementById("send");
 var notice = document.getElementById("notice");
 var msg = document.getElementById("msg");
- 
-var ws  = new WebSocket("ws://localhost:8001");
+
+var ws 	= new WebSocket("ws://localhost:8001");
 ws.onopen = function(){
-    notice.innerHTML = "websocket connected";
+	notice.innerHTML = "welcome";
 }
 ws.onclose = function(){
-    notice.innerHTML = "closed"
+	notice.innerHTML = "closed"
 }
 ws.onmessage = function(response){
-    console.log(response);
-    //收到message后，创建一个div，保存消息，并追加到msg中。
-    var div = document.createElement("div");
-    var json = JSON.parse(response.data);
-    switch(json.type){
-        case "enter":
-            div.innerHTML = "<font color='blue'>" + json.data + "</font>";
-            break;
-        case "message":
-            div.innerHTML = "<font color='orange'>" + json.data + "</font>";
-            break;
-        default:
-            div.innerHTML = "<font color='brown'>" + json.data + "</font>";
-            break;
-    }
-    msg.appendChild(div);
+	console.log(response);
+	//收到message后，创建一个div，保存消息，并追加到msg中。
+	showMsg(response);
 }
 send.onclick = function(){
-    ws.send(info.value)
+	if(info.value.trim().length){
+		ws.send(info.value)
+		info.value = "";//发送消息后，清空输入框内容
+	} else {
+		alert("内容不能为空")
+	}
+}
+
+function showMsg(response){
+	var div = document.createElement("div");
+	var json = JSON.parse(response.data);
+	switch(json.type){
+		case "enter":
+		case "left":
+			div.className = "system";
+			div.innerHTML = json.data;
+			break;
+		case "message":
+			div.className = "friend";
+			div.innerHTML = json.data;
+
+	}
+	msg.appendChild(div);
 }
